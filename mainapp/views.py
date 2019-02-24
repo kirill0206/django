76901688c40bffeaf89f392f58c35b625431868e
Kirill <arist84@mail.ru>
2019-02-24 23:28:53 +0300

    Commite after lesson1

---
+++ mainapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def main(request):
+	return render(request, 'mainapp/index.html')
+
+
+def catalog(request):
+	return render(request, 'mainapp/catalog.html')
+
+
+def contact(request):
+	return render(request, 'mainapp/contact.html')
